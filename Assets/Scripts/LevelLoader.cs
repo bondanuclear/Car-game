75d1f440150e55b3d03acccea85c882333e2f8dd
Andrii Bondarenko <lanbordik13@gmail.com>
2023-09-03 16:25:21 +0300

    Scene 3 UI added, controls changed to mobile

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -8,10 +8,13 @@ public class LevelLoader : MonoBehaviour
     RCC_CarControllerV3 car;
     RCC_Camera rcc_Camera;
     private void Awake() {
-        persistentData = FindObjectOfType<PersistentData>();
+        persistentData ??= FindObjectOfType<PersistentData>();
+        rcc_Camera = FindObjectOfType<RCC_Camera>();
     }
     private void Start() {
         //Debug.Log("Check value :" + persistentData.CarIndex);
-        car = RCC.SpawnRCC(persistentData.GetCar, Vector3.zero, Quaternion.identity, true, true, true);
+        car = RCC.SpawnRCC(persistentData.GetCar, new Vector3(0, 0.8f, 0), Quaternion.identity, true, true, true);
+        rcc_Camera.playerCar = car;
+        
     }
 }
