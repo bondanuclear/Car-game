bf38fac0a2af4dceb2bb4aecc7158413b53d1584
Andrii Bondarenko <lanbordik13@gmail.com>
2023-09-10 16:45:34 +0300

    minor changes

--- Assets/Scripts/CameraHandler.cs
+++ Assets/Scripts/CameraHandler.cs
@@ -9,7 +9,7 @@ public class CameraHandler : MonoBehaviour
 {
     [SerializeField] GameObject carLocation;
     [Header("Behaviour: only rotate around Y")]
-    [SerializeField] float rotatingSpeed = 5f;
+    //[SerializeField] float rotatingSpeed = 5f;
     float rotatingDirection;
     [SerializeField] Vector2 initialTouch;
     [SerializeField] Vector2 newPosition;
@@ -25,10 +25,10 @@ public class CameraHandler : MonoBehaviour
     Vector3 currVelocity = Vector3.zero;
     Vector3 currentRotation;
     [SerializeField] float smoothTime = 3;
-    [SerializeField] float maxSpeed = 100f;
+    //[SerializeField] float maxSpeed = 100f;
     
     private void Start() {
-       
+        currentRotation = new Vector3(14,-69,0);
     }
     // Update is called once per frame
     void Update()
@@ -37,35 +37,27 @@ public class CameraHandler : MonoBehaviour
         {
             Touch touch = Input.GetTouch(0);
             
-            //Debug.Log("Get touch" + Input.GetTouch(0).deltaPosition);
+            
             if (touch.phase == UnityEngine.TouchPhase.Began)
             {
                 initialTouch = touch.position;
-                //Debug.Log("initial touch" + initialTouch);
+                
             }
             else if (touch.phase == UnityEngine.TouchPhase.Moved)
             {
                 
                 Vector2 touchDeltaPosition = touch.deltaPosition;
                 //Vector2 deltaScaled = new Vector2(touchDeltaPosition.x / Screen.width, touchDeltaPosition.y / Screen.height);
-                //Debug.Log("delta: " + deltaScaled);    
+                   
                 rotationY += touchDeltaPosition.x  * sensitivity * Time.deltaTime;
                 rotationX -= touchDeltaPosition.y  * sensitivity * Time.deltaTime;
                 rotationX = Mathf.Clamp(rotationX, lowerThreshold, upperThreshold);
-                //Debug.Log(rotationX + " rotation X ");
-               
-                
-                // transform.localEulerAngles = Vector3.Lerp(transform.localEulerAngles,
-                // targetRotation, smoothTime);
-                //transform.localEulerAngles = Vector3.Slerp(transform.localEulerAngles, targetRotation, smoothTime);
-                // transform.localEulerAngles = Vector3.SmoothDamp(transform.localEulerAngles, targetRotation,
-                //   ref currVelocity, smoothTime); 
+                 
                 //RotateAroundY(touch);
                 
             }
             else if(touch.phase == UnityEngine.TouchPhase.Ended)
             {
-                
                 //rotatingDirection = 0;
             } 
             
